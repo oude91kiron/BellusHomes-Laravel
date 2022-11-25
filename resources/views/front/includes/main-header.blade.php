554db6176b06b698c9e8f96f061b5f150418e57b
OUDE ALOUDH <oude91kiron@users.noreply.github.com>
2022-11-25 04:55:17 +0300

    editing the view of the home page

---
+++ resources/views/front/includes/main-header.blade.php
@@ -0,0 +1,44 @@
+
+<!-- main header start -->
+<header class="main-header mt-4" id="main-header-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-lg navbar-light rounded">
+                    <a class="navbar-brand logo" href="#">
+                        <img src="{{asset('assets/front/img/logos/black-logo.png')}}" alt="logo">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="fa fa-bars"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbar">
+                        <ul class="navbar-nav justify-content-end ml-auto">
+                    
+                            <li class=" dropdown">
+                                <a href="#" type="button" class="btn btn-submit font-weight-bold">Home</a>
+                            </li>
+                            <li class=" dropdown">
+                                <a href="#" type="button" class="btn btn-submit font-weight-bold">About Us</a>
+                            </li>
+                            <li class=" dropdown">
+                                <a href="#" type="button" class="btn btn-submit font-weight-bold">Properties</a>
+                            </li>
+                            <li class=" dropdown">
+                                <a href="#" type="button" class="btn btn-submit font-weight-bold">Our Service</a>
+                            </li>
+                            <li class=" dropdown">
+                                <a href="#" type="button" class="btn btn-submit font-weight-bold">Contact Us</a>
+                            </li>
+                            <li class=" dropdown">
+                                <a href="fr" type="button" class="btn btn-submit font-weight-bold">
+                                    <i class="fa fa-language" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- main header end -->
---
+++ resources/views/front/includes/top-header.blade.php
@@ -0,0 +1,15 @@
+<!-- Top header start -->
+<header class="top-header th-bg" id="top-header-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <div class="list-inline">
+                    <a href="tel:1-7X0-555-8X22"><i class="fa fa-phone"></i>+0477 85X6 552</a>
+                    <a href="tel:info@themevessel.com"><i class="fa fa-envelope"></i>info@themevessel.com</a>
+                    <a href="#" class="mr-0"><i class="fa fa-clock-o"></i>Mon - Sun: 8:00am - 6:00pm</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Top header end -->
