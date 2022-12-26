8d54404b8d69e68f4ec63955c5077bd4b9d50088
OUDE ALOUDH <oude91kiron@users.noreply.github.com>
2022-12-26 11:46:39 +0300

    create post page done

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -38,7 +38,7 @@ class PostController extends Controller
      */
     public function create() {
 
-        return view('create');
+        return view('dashboard.posts.create');
     }
     //----------------------------------------------------------------------
 
--- resources/views/front/includes/banner.blade.php
+++ resources/views/front/includes/banner.blade.php
@@ -2,12 +2,16 @@
 <div class="banner" id="banner">
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
+
+            @isset($sliders)
+            @foreach ($sliders as $slider)
+            @if($loop->first)
             <div class="carousel-item active item-bg">
-                <img class="d-block w-100 h-100" src="{{$sliders[0]->photo}}" alt="banner">
+                <img class="d-block w-100 h-100" src="{{$slider->photo}}" alt="banner">
                 <div class="carousel-caption banner-slider-inner d-flex h-100 text-center">
                     <div class="carousel-content container b1-inner">
                         <div class="tab-search-section">
-                            <h1>Find Your Dream Properties</h1>
+                            <h1>Find Your Dream Home</h1>
                             <div id="typed-strings">
                                 <p>Find new & featured property located in istanbul city.</p>
                             </div>
@@ -15,13 +19,13 @@
                     </div>
                 </div>
             </div>
-
-            <div class="carousel-item  item-bg">
-                <img class="d-block w-100 h-100" src="{{$sliders[1]->photo}}" alt="banner">
+            @else
+            <div class="carousel-item item-bg">
+                <img class="d-block w-100 h-100" src="{{$slider->photo}}" alt="banner">
                 <div class="carousel-caption banner-slider-inner d-flex h-100 text-center">
                     <div class="carousel-content container b1-inner">
                         <div class="tab-search-section">
-                            <h1>Find Your Dream Properties</h1>
+                            <h1>Find Your Dream Home</h1>
                             <div id="typed-strings">
                                 <p>Find new & featured property located in istanbul city.</p>
                             </div>
@@ -29,21 +33,10 @@
                     </div>
                 </div>
             </div>
-
-            <div class="carousel-item  item-bg">
-                <img class="d-block w-100 h-100" src="{{$sliders[2]->photo}}" alt="banner">
-                <div class="carousel-caption banner-slider-inner d-flex h-100 text-center">
-                    <div class="carousel-content container b1-inner">
-                        <div class="tab-search-section">
-                            <h1>Find Your Dream Properties</h1>
-                            <div id="typed-strings">
-                                <p>Find new & featured property located in istanbul city.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
+            @endif
+            @endforeach
+            @endisset            
+  
         </div>
     </div>
 </div>
